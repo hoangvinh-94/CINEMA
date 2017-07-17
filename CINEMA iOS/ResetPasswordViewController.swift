b6f1fe8eee5c4168827d352ea7fdd0f041aa517e
CongllSpkt <thanhcongnguyen1995@gmail.com>
2017-07-17 16:14:28 -0700

    Formated code and convention for ResetPasswordViewController

--- CINEMA iOS/ResetPasswordViewController.swift
+++ CINEMA iOS/ResetPasswordViewController.swift
@@ -9,86 +9,78 @@
 import UIKit
 import Firebase
 
+
+// MARK: - ResetPasswordViewController
 class ResetPasswordViewController: UIViewController {
+    
     // MARK: - IBOutlet
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var menuButton: UIBarButtonItem!
+    
      // MARK: - Override func
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         menuButton.target = revealViewController()
         menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
-
         emailTextField.backgroundColor = UIColor.white
         emailTextField.layer.cornerRadius = 5
         emailTextField.layer.borderWidth = 1
         emailTextField.layer.borderColor = UIColor.blue.cgColor
-        
         UIGraphicsBeginImageContextWithOptions(self.view.frame.size, self.view.isOpaque, 0.5)
         UIImage(named: "nen18.jpg")?.draw(in: self.view.bounds)
-        
         let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        
         UIGraphicsEndImageContext()
-        
-        
-        
         self.view.backgroundColor = UIColor(patternImage: image)
         // Do any additional setup after loading the view.
     }
     
+    // sign in button clicked
     @IBAction func signInAction(_ sender: Any) {
-        let revealviewcontroller:SWRevealViewController = self.revealViewController()
         
+        let revealviewcontroller:SWRevealViewController = self.revealViewController()
         let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SignInViewController")
         let newFrontController = UINavigationController.init(rootViewController:vc)
         revealviewcontroller.pushFrontViewController(newFrontController, animated: true)
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
      // MARK: - Methods
+    // Confirm submited
     @IBAction func submitAction(_ sender: AnyObject) {
         
         if self.emailTextField.text == "" {
             let alertController = UIAlertController(title: "Oops!", message: "Please enter an email.", preferredStyle: .alert)
-            
             let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
             alertController.addAction(defaultAction)
-            
             present(alertController, animated: true, completion: nil)
-            
-        } else {
+        }
+        else {
             Auth.auth().sendPasswordReset(withEmail: self.emailTextField.text!, completion: { (error) in
-                
                 var title = ""
                 var message = ""
-                
                 if error != nil {
                     title = "Error!"
                     message = (error?.localizedDescription)!
-                } else {
+                }
+                else {
                     title = "Success!"
                     message = "Password reset email sent. Check your inbox!"
                     self.emailTextField.text = ""
                 }
-                
                 let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
                 
                 // add the actions (buttons)
                 alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: { action in
-                    
                     let homeView = self.storyboard?.instantiateViewController(withIdentifier: "SignInViewController") as! SignInViewController
                     self.navigationController?.pushViewController(homeView, animated: true)
-                    
                 }))
                 self.present(alertController, animated: true, completion: nil)
-                
-                
             })
         }
     }
-    
-    
 }
